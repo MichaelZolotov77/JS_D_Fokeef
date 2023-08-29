9a2f6a75017220cf6e4245b473aa4bfa426b433b
MichaelZolotov77 <michaelzol@yandex.ru>
2023-08-29 18:06:30 +0300

    2.8

---
+++ js/js.js
@@ -0,0 +1,20 @@
+const myNumber = 10
+
+console.log(typeof myNumber)//number
+
+let myNumber2
+
+console.log(typeof myNumber2)//undefined
+
+let myNumber3 = null
+
+console.log(typeof myNumber3)//object
+
+let myString = 'Mic'
+
+console.log(typeof myString)//string
+
+let isTrue = true
+
+console.log(typeof isTrue)//boolean
+
