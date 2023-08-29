1fc6a1447acd17ebcb141299bf6752a011cdd315
MichaelZolotov77 <michaelzol@yandex.ru>
2023-08-29 20:16:27 +0300

    2.12

--- js/js.js
+++ js/js.js
@@ -1,5 +1,21 @@
-console.log('hello' / 2)//NaN
+//сравнения
 
-const name = 'world'
+console.log('2' > 1)//true, строка преобразуется в число
 
-console.log(`Hello ${name}`)//Hello world
+console.log('01' == 1)//true
+
+console.log('Я' > 'А')//true, строки сравниваются посимвольно
+
+console.log('а' > 'Я')//true, строчные буквы имеют больший приоретет
+
+console.log(true > false)//true
+
+console.log('Hello10' > 5)//false, 'Hello10' конвертируется в NaN
+
+console.log(undefined == 0)//false, undefined конвертируется в NaN, NaN всегда выдает false
+
+console.log(undefined == null)//true, конвертации нет
+
+console.log(undefined === null)//false, плюс сравнение типов, типы разные
+
+console.log('10' === 10)//false
