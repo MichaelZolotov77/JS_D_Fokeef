5c9e6b9e80bb8c7a347d500fbcd25b898665772a
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-02 20:23:21 +0300

    3.11

--- js/js.js
+++ js/js.js
@@ -2,29 +2,21 @@
 
 const user = {
     name: 'John',
-    age: 30
+    age: 30,
+    placeOfBirth: {
+        country: 'Russia',
+        city: 'Kazan'
+    }
 }
 
-user.name = 'Anna'
 console.log(user)
 
-user.sex = 'male'
+user.placeOfBirth.country = 'USA'
 console.log(user)
 
-delete user.sex
+user.placeOfBirth['dateOfBirth'] = 1992
 console.log(user)
 
-user['sex'] = 'male'
+delete user['placeOfBirth'].city
 console.log(user)
 
-delete user['sex']
-console.log(user)
-
-const propertyName = 'newProperty'
-user[propertyName + ' 2'] = 'propertyValue'
-console.log(user)
-console.log(user['newProperty 2'])
-
-const answer = prompt('Напишите слово name или age')
-
-alert(user[answer])
