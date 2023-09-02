17dc3e1e02c2396e6dacdd91f7424bd44de87ace
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-02 21:42:28 +0300

    3.13.3

--- js/js.js
+++ js/js.js
@@ -3,15 +3,17 @@
 const firstName = 'John'
 const lastName = 'Lennon'
 
-function calcAge() {
-    return 2023 - user.birthYear
-}
+// function calcAge() {
+//     return 2023 - user.birthYear
+// }
 
 const user = {
     firstName,
     lastName,
     birthYear: 1940,
-    calcAge
+    calcAge() {
+        return 2023 - user.birthYear
+    }
 }
 
 console.log(user.calcAge())
