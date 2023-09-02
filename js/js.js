879dc2bbda8aa34b0a2e9d3660ade14768eb3f89
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-02 14:53:27 +0300

    3.8

--- js/js.js
+++ js/js.js
@@ -1,21 +1,21 @@
 'use strict'
 
 function ask(question, yes, no) {
-    if(confirm(question)) {
+    if (confirm(question)) {
         yes()
-    }else {
+    } else {
         no()
     }
 }
 
-function showOk(){
-    alert('Спасибо за согласие')
-}
+// function showOk(){
+//     alert('Спасибо за согласие')
+// }
 
-function showCancel(){
-    alert('Вы отменили выполнение')
-}
+// function showCancel(){
+//     alert('Вы отменили выполнение')
+// }
 
-ask('Вы согласны?', showOk, showCancel)
+// ask('Вы согласны?', showOk, showCancel)
 
-ask('Вы согласны?', function(){alert('Спасибо за согласие')}, function(){ alert('Вы отменили выполнение')})
+ask('Вы согласны?', () => alert('Спасибо за согласие'), () => alert('Вы отменили выполнение'))
