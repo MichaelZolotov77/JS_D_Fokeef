8a080f1ee0185da81e7371e48f4bed5941c53eb8
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-03 22:31:47 +0300

    4.1

--- js/js.js
+++ js/js.js
@@ -1,17 +1,4 @@
-'use strict'
+'use strict';
+let num = 5;
 
-function fibonachi(length) {
-    let arr = [0, 1]
-
-    if (length < 1) return 'Число должно быть больше нуля'
-    if (length == 1) return [0]
-    if (length == 2) return arr
-
-    for (let i = 3; i <= length; i++) {
-        let nextNum = arr.at(-1) + arr.at(-2)
-        arr.push(nextNum)
-    }
-    return arr
-}
-
-console.log(fibonachi(2))
+console.log(`num ${num}`);
