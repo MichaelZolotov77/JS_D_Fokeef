f2884e82083248ccfb6e29ee155d62a796d5febc
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-04 15:05:16 +0300

    4.3

--- js/js.js
+++ js/js.js
@@ -1,4 +1,12 @@
 'use strict';
-let num = 5;
 
-console.log(`num ${num}`);
+//получение элементов
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('.header'));
+console.log(document.querySelector('ul'));
+console.log(document.querySelector('li'));
+console.log(document.querySelectorAll('.nav__item'));
+
+let header = document.querySelector('h1');
+
+console.log(header);
