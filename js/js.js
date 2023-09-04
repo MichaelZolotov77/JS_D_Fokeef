ee3819d889b8f38bf3db6043f07303564e840d70
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-04 21:48:39 +0300

    4.5.1

--- js/js.js
+++ js/js.js
@@ -7,3 +7,9 @@ let arr = [1, 2, 3, 4, 5];
 console.log(navItems); //Prototype : NodeList
 
 console.log(arr); //Prototype : Array
+
+let arr2 = Array.from(navItems); //превращение в обычный массив
+
+arr2.pop(); //работает
+
+console.log(arr2);
