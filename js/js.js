26ca4ae21a3990fb4665d7c2f6ba60e706b064be
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-05 21:34:55 +0300

    4.7.1

--- js/js.js
+++ js/js.js
@@ -1,33 +1,14 @@
 'use strict';
 
-let arr = ['hello', 2, true, 4];
+let elem = document.querySelector('h1');
 
-for (let value of arr) {
-  console.log(value); //получаем значения
-}
+console.log(elem.innerHTML);
 
-let obj = {
-  name: 'John',
-  lastName: 'Travolta',
-  age: 30,
-};
+let oldData = elem.innerHTML;
 
-for (let key in obj) {
-  console.log(key); //получаем ключи
-}
+elem.innerHTML = `<p>${oldData}</p><p>Привет всем! Это новый текст</p>`;
 
-for (let key in obj) {
-  console.log(obj[key]); //получаем значения
-}
+console.log(elem.innerHTML);
 
-let arr2 = document.querySelectorAll('li');
-
-console.log(arr2);
-
-for (let key in arr2) {
-  console.log(arr2[key]); //такой метод перебора для псевдомассивов не подходит
-}
-
-for (let value of arr2) {
-  console.log(value); //получаем значения псевдомассива
-}
+//выводит просто текст
+//elem.textContent = `<p>${oldData}</p><p>Привет всем! Это новый текст</p>`;
