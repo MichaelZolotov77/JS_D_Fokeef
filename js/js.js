84b9649659ecac1bceee61c275863b8d66d44a0f
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-06 21:58:30 +0300

    4.9

--- js/js.js
+++ js/js.js
@@ -2,10 +2,7 @@
 
 let header = document.querySelector('h1');
 let secondHeader = document.createElement('h2');
-let thirdHeader = document.createElement('h3');
 
 secondHeader.innerHTML = 'Заголовок второго уровня';
-thirdHeader.innerHTML = 'Заголовок третьего уровня';
-header.before(thirdHeader);
-header.append(secondHeader);
-console.log(secondHeader);
+
+header.insertAdjacentHTML('afterbegin', '<p>Some text</p>');
