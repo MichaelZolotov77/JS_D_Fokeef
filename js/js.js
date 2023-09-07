8310340529fec37dd8d963a0d48942cf9d9b2b29
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-07 20:06:15 +0300

    4.10.1

--- js/js.js
+++ js/js.js
@@ -1,8 +1,6 @@
 'use strict';
 
 let header = document.querySelector('h1');
+let ul = document.querySelector('ul');
 
-header.insertAdjacentHTML('afterbegin', '<p>Afterbegin</p>');
-header.insertAdjacentHTML('beforebegin', '<p>Beforebegin</p>');
-header.insertAdjacentHTML('beforeend', '<p>Beforeend</p>');
-header.insertAdjacentHTML('afterend', '<p>Afterend</p>');
+ul.prepend(header);
