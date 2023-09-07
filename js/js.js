5463ca3441fde650baa117e53b6c91572b986de6
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-07 21:05:54 +0300

    4.11.1

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 'use strict';
 
 let header = document.querySelector('h1');
-let ul = document.querySelector('ul');
 
-header.remove();
+header.style.marginBottom = '100px';
+header.style.fontSize = '100px';
