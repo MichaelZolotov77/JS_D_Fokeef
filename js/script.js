92fcdc0db00bb4fa713dcefe17202cd72e8b382b
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-10 21:04:28 +0300

    5.2

--- js/script.js
+++ js/script.js
@@ -1,38 +1,44 @@
 'use strict';
-/*
-ЗАДАЧА:
-Создайте игру "Собери алфавит"
 
-ПОДСКАЗКИ:
+const user = {
+  name: 'John',
+  age: 30,
+  calcAge: function () {
+    // Первый способ через создание переменной
+    // const self = this;
+    // console.log(this);
+    // function newFn() {
+    //   console.log(self);
+    // }
 
-1-Не думайте о сокращении кода. Если будет очень много повторений однотипного кода - это нормально. Первая задача понимать логику языка, а только после оптимизировать его.
-2-Помните про методы переноса элемента из одного места в другое(before, after и т.д.)
-3-Помните про свойства, получения соседних элементов (previousElementSibling, previousElementSibling и т.д)
+    // Второй способ через стрелочную функцию
+    const newFn = () => {
+      console.log(this);
+    };
+    newFn();
+  },
+};
 
-*/
+user.calcAge();
 
-const elements = document.querySelectorAll('.boxes__box');
-const mountainAudio = new Audio('audio/Mountain Audio - Menu Click.mp3');
-const huge = new Audio('audio/huge.wav');
-let previousElement;
-let newLetters;
+// function calcAge() {
+//   console.log(this);
+// }
 
-for (let elem of elements) {
-  elem.addEventListener('click', function () {
-    previousElement = elem.previousElementSibling;
-    previousElement.before(elem);
-    mountainAudio.play();
-    newLetters = document.querySelectorAll('.boxes__box');
-    console.log(newLetters);
-    if (
-      newLetters[0].classList.contains('box_a') &&
-      newLetters[1].classList.contains('box_b') &&
-      newLetters[2].classList.contains('box_c') &&
-      newLetters[3].classList.contains('box_d') &&
-      newLetters[4].classList.contains('box_e') &&
-      newLetters[5].classList.contains('box_f')
-    ) {
-      huge.play();
-    }
-  });
-}
+// calcAge();
+
+// const myFn = () => {
+//   console.log(this);
+// };
+
+// myFn();
+
+// document.querySelector('h1').addEventListener('click', function () {
+//   this.style.color = 'red';
+//   console.log(this);
+// });
+
+// document.querySelector('h1').addEventListener('click', () => {
+//   //this.style.color = 'red';
+//   console.log(this);
+// });
