cf15661c6e921f4a45b2aa9d3a27bbd49255cf60
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-11 19:04:05 +0300

    5.7

--- js/script.js
+++ js/script.js
@@ -1,29 +1,90 @@
 'use strict';
+/*
+ЗАДАЧИ:
 
-const obj = {
-  fruits: ['apple', 'orange', 'limon', 'watermelon'],
-  drinks: ['juce', 'tea', 'coctail'],
-  food: {
-    soup: 'borch',
-    pasta: 'spagetti',
+1.Создайте отдельные массивы игроков каждой команды. (Переменные pleayersTeamOne и pleayersTeamTwo)
+
+2. Первый игрок в каждом массиве - это вратарь, остальные игроки это просто члены команды. Для первых игроков каждой команды, создайте переменную goalKeeper, а для всех остальных fieldPlayers.
+
+3.Создайте один массив allPlayers который будет содержать всех игроков обеих команд.
+
+4.Добавьте в массив allPlayers еще 3-х игроков. (Имена игроков придумайте сами)
+
+5. В объекте game есть объект odds, внутри которого три свойства: 
+team1: 1.33, 
+x: 3.25,
+team2: 6.5. 
+С помощью деструктуризации объекта, создайте 3 переменные из этого объекта. При создании переменных, 
+let team1 = 1.33,
+let x = 3.25,
+let team2 = 6.5
+поменяйте имя свойства x на draw.
+
+
+
+
+*/
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
+//1
+let [pleayersTeamOne, pleayersTeamTwo] = game.players;
+console.log(pleayersTeamOne);
+console.log(pleayersTeamTwo);
 
-const { fruits, ...other } = obj;
-console.log(fruits);
-console.log(other);
+//2
+let [goalKeeperTeamOne, ...fieldPlayersTeamOne] = pleayersTeamOne;
+console.log(goalKeeperTeamOne);
+console.log(fieldPlayersTeamOne);
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
-const [a, b, c, ...others] = arr;
-console.log(a, b, c, others);
+let [goalKeeperTeamTwo, ...fieldPlayersTeamTwo] = pleayersTeamTwo;
+console.log(goalKeeperTeamTwo);
+console.log(fieldPlayersTeamTwo);
 
-function calc(a, b, c, ...numbers) {
-  let sum = 0;
-  for (let value of numbers) {
-    sum += value;
-  }
-  console.log(a, b, c);
-  console.log(sum);
-}
+//3
+let allPlayers = [...pleayersTeamOne, ...pleayersTeamTwo];
+console.log(allPlayers);
 
-calc(21, 3, 45, 6, 1, 2, 3);
+//4
+let allPlayersAdd = [...allPlayers, 'Ivanov', 'Petrov', 'Sidorov'];
+console.log(allPlayersAdd);
+
+//5
+let {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
