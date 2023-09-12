02af51b40b322d4847c1613b37266be03f4e936f
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-12 21:54:39 +0300

    5.10

--- js/script.js
+++ js/script.js
@@ -1,27 +1,14 @@
 'use strict';
 
-const names = new Array(
-  'Дмитрий',
-  'Иван',
-  'Виктор',
-  'Анастасия',
-  'Игорь',
-  'Дмитрий',
-  'Иван',
-  'Анастасия'
-);
+const hotel = new Map();
 
-const uniqNames = new Set(names);
-uniqNames.delete('Дмитрий');
-console.log(...uniqNames);
-
-const updateNames = Array.from(uniqNames);
-console.log(updateNames);
-
-const name = 'Dmitrii';
-const uniqLetters = new Set(name);
-console.log(uniqLetters);
-
-for (let value of uniqLetters) {
-  console.log(value);
-}
+hotel.set('categories', ['std', 'eco', 'suit', 'lux']);
+hotel.set(true, 'Мы открыты');
+hotel.set(false, 'Мы закрыты');
+const arr = [2, 3];
+hotel.set(arr, 'Это массив');
+console.log(hotel);
+console.log(hotel.get(true));
+console.log(hotel.get(arr));
+hotel.delete(arr);
+console.log(hotel);
