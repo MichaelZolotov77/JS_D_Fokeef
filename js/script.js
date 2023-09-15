a497dedc6192409d3d85e8104b1f098d3aa96c0e
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-15 21:00:46 +0300

    7.2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 'use strict';
 
-const bookings = [];
-function createBooking(
-  flightNum,
-  numPassengers = 1,
-  price = 100 * numPassengers
-) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+const flight = '432RTN';
+const passenger = {
+  name: 'Mic',
+  passport: 392098863,
+};
+
+function checkIn(flightNum, passId) {
+  flightNum = '111RRR';
+  passId.name = 'Mr ' + passId.name;
+  if (passId.passport == 392098863) {
+    console.log('Вы успешно прошли регистрацию');
+  } else {
+    console.log('Регистрация не удалась');
+  }
 }
 
-createBooking('434KLM', 5);
+function newPassport(person) {
+  person.passport = Math.trunc(Math.random() * 1000000);
+  console.log(person.passport);
+}
+
+newPassport(passenger);
+checkIn(flight, passenger);
