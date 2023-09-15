8a2485f3377e096a362eb5d81f0d70d0cc1e0466
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-15 19:18:31 +0300

    7.1

--- js/script.js
+++ js/script.js
@@ -1,36 +1,18 @@
 'use strict';
 
-/*
-ЗАДАЧИ:
+const bookings = [];
+function createBooking(
+  flightNum,
+  numPassengers = 1,
+  price = 100 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+}
 
-Создайте программу которая будет преобразовывать переменные слова в которых разделены нижним тире, в переменные которые будут записанны в camelCase нотации.
-/////////
-
-Подсказки:
-1) Решение должно работать с переменными из 2-х слов. Не больше
-2)Чтобы получить строку введенных данных из textarea, можно получить значение свойства value, DOM элемента textarea
-3) Практика сложная, поэтому если на чем то застряли, посмотрите решение проблемы и пробуйте дальше самостоятельно.
-4)Записать результат вы можете в div с классом output, через innerText
-5)  По итогу переменные должны выглядеть так: 
-underscoreCase
-firstName
-someVariable
-calculateAge
-delayedDeparture
-
-*/
-
-document.querySelector('.btn').addEventListener('click', function () {
-  const text = document.querySelector('.text').value;
-  const rows = text.split('\n');
-  console.log(rows);
-  let output = [];
-
-  for (let row of rows) {
-    const [first, second] = row.trim().toLocaleLowerCase().split('_');
-    output.push(
-      `${first}${second.replace(second[0], second[0].toUpperCase())}`
-    );
-  }
-  document.querySelector('.output').innerText = output.join('\n');
-});
+createBooking('434KLM', 5);
