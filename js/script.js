9fd2d7fb6c2bbc4e035742fc079a08de3507bb24
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-16 17:33:50 +0300

    7.4

--- js/script.js
+++ js/script.js
@@ -1,25 +1,13 @@
 'use strict';
 
-//функция первого класса
-function oneWord(str) {
-  return str.replaceAll(' ', '').toLocaleLowerCase();
-}
-
-console.log(oneWord('Jdlln lkdkKKkl   dlkjf'));
-
-//функция первого класса
-function upperFirstLetter(str) {
-  const [first, ...other] = str.split(' ');
-  return [first[0].toUpperCase() + first.slice(1), ...other].join(' ');
-}
-
-console.log(upperFirstLetter('dkjlkj kjfpwi kdjf kjwk'));
-
 //функция высшего порядка
-function transformer(str, fn) {
-  console.log(`Это оригинальная строка: ${str}`);
-  console.log(`Измененная строка: ${fn(str)}`);
+function hello(hiStr) {
+  return function (name) {
+    console.log(`${hiStr} ${name}`);
+  };
 }
 
-transformer('превет, я очень люблю JS', upperFirstLetter);
-transformer('превет, я очень люблю JS', oneWord);
+const greeter = hello('Hi');
+greeter('Mic');
+
+hello('Hello')('Michael');
