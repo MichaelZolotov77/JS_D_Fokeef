93e320a182cee6c058c3bd73c036071ce35700ba
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-16 18:25:31 +0300

    7.5

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 'use strict';
 
-// function person() {
-//   console.log(this.firstName + ' ' + this.lastName);
-// }
+function person(greetStr) {
+  console.log(greetStr + ' ' + this.firstName + ' ' + this.lastName);
+}
 
 const person1 = {
   firstName: 'John',
   lastName: 'Doe',
-  person: function () {
-    console.log(this.firstName + ' ' + this.lastName);
-  },
 };
 
 const person2 = {
@@ -17,4 +14,30 @@ const person2 = {
   lastName: 'Doe',
 };
 
-person1.person.call(person2);
+person.call(person1, 'Good morning,');
+
+const s7 = {
+  airLine: 's7',
+  iCode: 'S7',
+  booking: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} забронировал место на самолет компании ${this.airLine}, номер рейса ${this.iCode}${flightNum}`
+    );
+    this.booking.push({ flight: `${this.iCode}${flightNum}`, name });
+  },
+};
+
+s7.book(2131, 'Mikhail');
+s7.book(5467, 'Dmitrii');
+
+const book = s7.book;
+
+const turkish = {
+  airLine: 'tr',
+  iCode: 'TR',
+  booking: [],
+};
+
+book.call(turkish, 1122, 'Ivan');
+console.log(turkish.booking);
