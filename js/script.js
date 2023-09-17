590ab94224931b70f198d96271719622e69d9391
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-17 12:55:47 +0300

    8.2

--- js/script.js
+++ js/script.js
@@ -1,41 +1,16 @@
 'use strict';
 
-const s7 = {
-  airLine: 's7',
-  iCode: 'S7',
-  booking: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} забронировал место на самолет компании ${this.airLine}, номер рейса ${this.iCode}${flightNum}`
-    );
-    this.booking.push({ flight: `${this.iCode}${flightNum}`, name });
-  },
-};
+const arr1 = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['f', 'g'];
 
-s7.book(2131, 'Mikhail');
-s7.book(5467, 'Dmitrii');
+console.log(arr1.slice(2, 4));
+console.log(arr1); //не изменился
 
-const book = s7.book;
+// arr2 = arr1.slice(0); //копия
+// arr3 = [...arr1]; //тоже копия
 
-const turkish = {
-  airLine: 'tr',
-  iCode: 'TR',
-  booking: [],
-};
+console.log(arr1.reverse());
+console.log(arr1); //revers меняет исходный массив
 
-book.call(turkish, 1122, 'Ivan');
-console.log(turkish.booking);
-
-const bookTr = s7.book.bind(turkish, 1111111);
-bookTr('Vika');
-
-turkish.planes = 300;
-turkish.buyPlanes = function () {
-  console.log(this);
-  this.planes++;
-  console.log(this.planes);
-};
-
-document
-  .querySelector('.btn')
-  .addEventListener('click', turkish.buyPlanes.bind(turkish));
+console.log(arr1.concat(arr2)); //делает копии, исходные массивы сохраняются
+// console.log([...arr1, ...arr2]);//тоже самое
