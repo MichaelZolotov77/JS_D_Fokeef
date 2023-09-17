986a9974fb56fbf0d46a75473274c8d9576f6e0e
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-17 18:05:03 +0300

    8.5

--- js/script.js
+++ js/script.js
@@ -1,30 +1,72 @@
 'use strict';
 
-const map = new Map([
-  ['USD', 'Dollars'],
-  ['RUB', 'Rubbles'],
-  ['EUR', 'Euro'],
-]);
+const account1 = {
+  owner: 'Dmitrii Fokeev',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  pin: 1111,
+};
 
-map.forEach(function (value) {
-  console.log(value);
-});
+const account2 = {
+  owner: 'Anna Filimonova',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  pin: 2222,
+};
 
-map.forEach(function (value, key) {
-  console.log(key);
-  console.log(value);
-});
+const account3 = {
+  owner: 'Polina Filimonova',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  pin: 3333,
+};
 
-map.forEach(function (value, key, map) {
-  console.log(key);
-  console.log(value);
-  console.log(map);
-});
+const account4 = {
+  owner: 'Stanislav Ivanchenko',
+  movements: [430, 1000, 700, 50, 90],
+  pin: 4444,
+};
 
-const set = new Set(map);
+const accounts = [account1, account2, account3, account4];
 
-set.forEach(function (value, key, set) {
-  console.log(key);
-  console.log(value);
-  console.log(set);
-});
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+function displayMovements(movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (value, i) {
+    const type = value > 0 ? 'deposit' : 'withdrawal';
+    const textType = value > 0 ? 'зачисление' : 'снятие';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">
+            ${i + 1} ${textType}
+          </div>
+          <div class="movements__date">24/01/2037</div>
+          <div class="movements__value">${value}</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+displayMovements(account1.movements);
