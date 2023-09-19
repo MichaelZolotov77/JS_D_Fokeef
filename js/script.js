39f7290e73fa3fe0ebf6e503856a48cc4ae3f1cd
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-19 10:05:13 +0300

    8.12

--- js/script.js
+++ js/script.js
@@ -111,3 +111,17 @@ function calcDisplaySum(movements) {
 }
 
 calcDisplaySum(account1.movements);
+
+//////////
+
+const arr = [10, 5, -2, -5, 23, -7];
+
+const firstMinusNum = arr.find(function (num) {
+  return num < 0;
+});
+console.log(firstMinusNum);
+
+const acc = accounts.find(function (acc) {
+  return acc.owner == 'Anna Filimonova';
+});
+console.log(acc);
