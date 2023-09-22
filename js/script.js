be8f20e4bd811b207421584a18a0ae9996e3342e
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-22 19:38:24 +0300

    9.1

--- js/script.js
+++ js/script.js
@@ -1,206 +1,26 @@
 'use strict';
 
-const account1 = {
-  owner: 'Dmitrii Fokeev',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  pin: 1111,
-};
+console.log(Number('1234')); // Преобразование в числа
+console.log(+'1234');
+console.log(+true); //1
+console.log(+false); //0
+console.log(Number.parseInt('12px')); //12
+console.log(Number.parseInt('12.557px')); //12, не работает с дробными числами
+console.log(Number.parseFloat('12.557px')); //12.557
 
-const account2 = {
-  owner: 'Anna Filimonova',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  pin: 2222,
-};
+console.log(Math.max(10, 5, 7)); //10
+console.log(Math.min(10, 15, 17)); //10
 
-const account3 = {
-  owner: 'Polina Filimonova',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Stanislav Ivanchenko',
-  movements: [430, 1000, 700, 50, 90],
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
-
-// Вывод на страницу всех приходов и расходов. Сортировка транзакций
-function displayMovements(movements, sort = false) {
-  containerMovements.innerHTML = '';
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
-
-  movs.forEach(function (value, i) {
-    const type = value > 0 ? 'deposit' : 'withdrawal';
-    const textType = value > 0 ? 'зачисление' : 'снятие';
-    const html = `
-    <div class="movements__row">
-          <div class="movements__type movements__type--${type}">
-            ${i + 1} ${textType}
-          </div>
-          <div class="movements__date">24/01/2037</div>
-          <div class="movements__value">${value} ₽</div>
-        </div>
-    `;
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-  });
+// В JavaScript нет функции генерации случайных чисел в диапазоне
+// Такую функцию пишут вручную
+function calcRandom(min, max) {
+  return Math.trunc(Math.random() * (max - min) + 1) + min;
 }
 
-// Создание логина из ФИО в объекте
-function createLogIn(accs) {
-  accs.forEach(function (acc) {
-    acc.logIn = acc.owner
-      .toLowerCase()
-      .split(' ')
-      .map(function (val) {
-        return val[0];
-      })
-      .join('');
-  });
-}
-createLogIn(accounts);
+console.log(calcRandom(10, 20));
 
-// Подсчет и вывод на страницу общего баланса
-function calcPrintBalance(acc) {
-  acc.balance = acc.movements.reduce(function (acc, val) {
-    return acc + val;
-  });
-
-  labelBalance.textContent = `${acc.balance} ₽`;
-}
-
-// Вывод прихода, расхода и суммы в footer
-function calcDisplaySum(movements) {
-  const incomes = movements
-    .filter((mov) => mov > 0)
-    .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes} ₽`;
-
-  const out = movements
-    .filter((mov) => mov < 0)
-    .reduce((acc, mov) => acc + mov, 0);
-
-  labelSumOut.textContent = `${Math.abs(out)} ₽`;
-  labelSumInterest.textContent = `${incomes + out} ₽`;
-}
-
-function updateUi(acc) {
-  displayMovements(acc.movements);
-  calcPrintBalance(acc);
-  calcDisplaySum(acc.movements);
-}
-
-// Авторизация
-let currentAccount;
-btnLogin.addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log('LogIn');
-  currentAccount = accounts.find(function (acc) {
-    return acc.logIn == inputLoginUsername.value;
-  });
-  console.log(currentAccount);
-  if (currentAccount && currentAccount.pin == Number(inputLoginPin.value)) {
-    containerApp.style.opacity = 100;
-    inputLoginPin.value = inputLoginUsername.value = '';
-    console.log('Pin ok');
-    updateUi(currentAccount);
-  }
-});
-
-// Перевод денег
-btnTransfer.addEventListener('click', function (e) {
-  e.preventDefault();
-  const reciveAcc = accounts.find(function (acc) {
-    return acc.logIn === inputTransferTo.value;
-  });
-  const amount = Number(inputTransferAmount.value);
-  console.log(amount, reciveAcc);
-  if (
-    reciveAcc &&
-    amount > 0 &&
-    currentAccount.balance >= amount &&
-    reciveAcc.logIn !== currentAccount.logIn
-  ) {
-    currentAccount.movements.push(-amount);
-    reciveAcc.movements.push(amount);
-    updateUi(currentAccount);
-    inputTransferTo.value = inputTransferAmount.value = '';
-  }
-});
-
-// Закрыть аккаунт
-btnClose.addEventListener('click', function (e) {
-  e.preventDefault();
-  if (
-    inputCloseUsername.value === currentAccount.logIn &&
-    Number(inputClosePin.value) === currentAccount.pin
-  ) {
-    const index = accounts.findIndex(function (acc) {
-      return acc.logIn === currentAccount.logIn;
-    });
-    console.log(index);
-    accounts.splice(index, 1);
-    containerApp.style.opacity = 0;
-    console.log(accounts);
-  }
-  inputCloseUsername.value = inputClosePin.value = '';
-});
-
-// Внести деньги
-btnLoan.addEventListener('click', function (e) {
-  e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
-  if (amount > 0) {
-    currentAccount.movements.push(amount);
-    updateUi(currentAccount);
-  }
-  inputLoanAmount.value = '';
-});
-
-// Посчитать сумму всех баллансов аккаунов
-const overalBalance = accounts
-  .map((acc) => acc.movements)
-  .flat()
-  .reduce((acc, mov) => acc + mov, 0);
-
-// Сортировка транзакций
-let sorted = false;
-btnSort.addEventListener('click', function (e) {
-  e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
-  sorted = !sorted;
-});
-
-// Замена значка на транзакциях
-labelBalance.addEventListener('click', function () {
-  Array.from(document.querySelectorAll('.movements__value'), function (val, i) {
-    return (val.innerText = val.textContent.replace('₽', 'RUB'));
-  });
-});
+console.log(Math.round(2.5)); //3, округление до ближайшего целого
+console.log(Math.ceil(2.1)); //3, округление в большую сторону
+console.log(Math.floor(2.9)); //2, округление в меньшую сторону
+console.log(+(2.345664).toFixed(2)); //2.34 Возвращает строку!
+// Если хотим вернуть число, поставить плюс впереди числа (или Number)
