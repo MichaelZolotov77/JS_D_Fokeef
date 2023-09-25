6437f8ff70eaed74c80539e9b55e277dcfe51710
MichaelZolotov77 <michaelzol@yandex.ru>
2023-09-25 21:35:35 +0300

    10.4.2

--- script.js
+++ script.js
@@ -43,15 +43,30 @@ btnScroll.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-// Отмена событий
-const h1 = document.querySelector('h1');
-
-function alertH1() {
-  alert('Hello');
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-h1.addEventListener('mouseenter', alertH1);
+function randomColor() {
+  return `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+}
+console.log(randomColor());
 
-setTimeout(function () {
-  h1.removeEventListener('mouseenter', alertH1);
-}, 4000);
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelector('.nav__links');
+const link = document.querySelector('.nav__link');
+
+nav.addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(e.target);
+  console.log(e.currentTarget == this);
+});
+
+navLinks.addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+
+link.addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  //e.stopPropagation();// Запрещает всплытие событий
+});
