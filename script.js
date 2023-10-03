0823cd4a3bccd1b21e7474870b36b03fea4bf83d
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-03 19:20:42 +0300

    11.5

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 'use strict';
 
-function Car(name, speed) {
-  this.name = name;
-  this.speed = speed;
+function User(firstname, lastname, age) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.age = age;
 }
-Car.prototype.plusSpeed = function () {
-  console.log(`Авто ${this.name}, скорость ${(this.speed += 10)} км/ч`);
+User.prototype.calcage = function () {
+  return 2023 - this.age;
 };
+const ivan = new User('Ivan', 'Ivanov', 46);
+console.log(ivan);
+console.log(ivan.calcage());
 
-Car.prototype.minusSpeed = function () {
-  console.log(`Авто ${this.name}, скорость ${(this.speed -= 5)} км/ч`);
-};
+class User1 {
+  constructor(firstname, lastname, age) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+  }
+  calcage() {
+    return 2023 - this.age;
+  }
+}
 
-const lada = new Car('Лада', 170);
-const skoda = new Car('Skoda', 105);
-
-lada.plusSpeed();
-lada.plusSpeed();
-lada.minusSpeed();
-skoda.minusSpeed();
-skoda.minusSpeed();
+const valentina = new User1('Valentina', 'Popova', 92);
+console.log(valentina.calcage());
