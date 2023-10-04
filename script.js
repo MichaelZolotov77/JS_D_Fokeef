3860145adef6c99e0cbb0a89b3c04aa8d41d8b14
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-04 20:58:02 +0300

    11.8

--- script.js
+++ script.js
@@ -1,49 +1,31 @@
 'use strict';
 
-class User {
-  constructor(firstname, lastname, birthYear) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.age;
-    this.birthYear = birthYear;
+class Car {
+  constructor(mark, speed) {
+    this.mark = mark;
+    this.speed = speed;
+    this.speedUs;
   }
-  get birthYear() {
-    return new Date().getFullYear() - this.age;
+
+  get speed() {
+    return this.speedUs * 1.6;
   }
-  set birthYear(val) {
-    const year = '' + val;
-    if (year.length < 4) {
-      alert('Вы ввели меньше 4-х символов');
-      return;
-    }
-    this.age = new Date().getFullYear() - year;
+
+  set speed(val) {
+    this.speedUs = val / 1.6;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`Авто ${this.mark}, скорость ${this.speed} км/ч`);
+  }
+
+  break() {
+    this.speed -= 5;
+    console.log(`Авто ${this.mark}, скорость ${this.speed} км/ч`);
   }
 }
 
-const anna = new User('Anna', 'Filimonova', 1990);
-console.log(anna);
-
-///////////////////////
-
-class User1 {
-  constructor(fullName) {
-    this.firstName;
-    this.lastName;
-    this.fullName = fullName;
-  }
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  set fullName(val) {
-    if (val.length < 3) {
-      alert('Короткое имя');
-      return;
-    }
-    const name = val.split(' ');
-    this.firstName = name[0];
-    this.lastName = name[1];
-  }
-}
-
-const anna1 = new User1('Anna Filimonova');
-console.log(anna1);
+const lada = new Car('Lada', 100);
+lada.accelerate();
+console.log(lada);
