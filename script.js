0f1156b717fa25329d2d6fda33ed36cbb98138f2
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-06 19:36:33 +0300

    11.9

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
 'use strict';
 
-class Car {
-  constructor(mark, speed) {
-    this.mark = mark;
-    this.speed = speed;
-    this.speedUs;
+class Article {
+  constructor(title, date) {
+    this.title = title;
+    this.date = date;
   }
-
-  get speed() {
-    return this.speedUs * 1.6;
-  }
-
-  set speed(val) {
-    this.speedUs = val / 1.6;
-  }
-
-  accelerate() {
-    this.speed += 10;
-    console.log(`Авто ${this.mark}, скорость ${this.speed} км/ч`);
-  }
-
-  break() {
-    this.speed -= 5;
-    console.log(`Авто ${this.mark}, скорость ${this.speed} км/ч`);
+  static createTodays() {
+    return new this('Some article', new Date());
   }
 }
 
-const lada = new Car('Lada', 100);
-lada.accelerate();
-console.log(lada);
+console.log(Article.createTodays());
