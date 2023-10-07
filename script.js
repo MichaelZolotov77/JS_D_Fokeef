d2bc1edb245d297e6cdb5a418480411a9d45bb43
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-07 14:34:53 +0300

    11.11

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 'use strict';
 
-const newProto = {
-  calcBirth() {
-    console.log(2023 - this.age);
-  },
+function Employee(firstName, lastName, age, post) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.post = post;
+}
 
-  init(firstName, age) {
-    this.firstName = firstName;
-    this.age = age;
-  },
+Employee.prototype.calcBirth = function () {
+  console.log(new Date().getFullYear() - this.age);
 };
 
-const ivan = Object.create(newProto);
-ivan.init('Ivan', 35);
+const cashier = new Employee('Ivan', 'Avdeev', 26, 'cashier');
+console.log(cashier);
+cashier.calcBirth();
 
-console.log(ivan);
-ivan.calcBirth();
+/////////////
+
+function Manager(firstName, lastName, age, post, password) {
+  Employee.call(this, firstName, lastName, age, post);
+  this.password = password;
+}
+
+Manager.prototype = Object.create(Employee.prototype);
+
+// Новый метод создается после создания прототипа
+Manager.prototype.sayHello = function () {
+  console.log('Привет, я метод менеджера');
+};
+
+const manager = new Manager('Irina', 'Petrova', 33, 'manager', 'pass');
+manager.calcBirth();
+console.log(manager);
+manager.sayHello();
