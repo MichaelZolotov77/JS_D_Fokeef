6442ea80fd0f135e0565dcc92f10494a19a1deaf
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-08 22:02:41 +0300

    12.2

--- script.js
+++ script.js
@@ -1,48 +1,36 @@
 'use strict';
 
-// ЗАДАЧА:
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
-// 1.Перепешите код из прошлой практики в синтаксис классов.
-// 2.Сделайте свойство charge приватным.
-// 3.Сделайте так чтобы методы chargeBattery и accelerate можно было вызывать по цепочке.
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
 
-class Car {
-  constructor(mark, speed) {
-    this.mark = mark;
-    this.speed = speed;
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    console.log(position);
+    console.log(latitude, longitude);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  },
+  function () {
+    alert('Вы не предоставили доступ к своей локации');
   }
-  accelerate() {
-    this.speed += 10;
-    console.log(`${this.mark} едедет со скоростью ${this.speed} км\ч`);
-  }
-  break() {
-    this.speed -= 5;
-    console.log(`${this.mark} едедет со скоростью ${this.speed} км\ч`);
-  }
-}
-
-class ElectroCar extends Car {
-  #charge;
-  constructor(mark, speed, charge) {
-    super(mark, speed);
-    this.#charge = charge;
-  }
-  accelerate() {
-    this.speed += 20;
-    this.#charge--;
-    console.log(
-      `${this.mark} едет со скоростью ${this.speed} , с уровнем заряда ${
-        this.#charge
-      } `
-    );
-    return this;
-  }
-  chargeBattery(chargeTo) {
-    this.#charge = chargeTo;
-    return this;
-  }
-}
-
-const tesla = new ElectroCar('Tesla', 100, 50);
-tesla.chargeBattery(95).accelerate();
-console.log(tesla);
+);
