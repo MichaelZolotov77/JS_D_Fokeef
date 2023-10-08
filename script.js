139dd900136254dc4cf68f9cc9a4e15727be376c
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-08 14:41:15 +0300

    11.16

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 'use strict';
 
 class Account {
-  movements = [];
+  _movements = [];
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
+    this._pin = pin;
     console.log('Вы создали аккаунт');
   }
   sayHi() {
     console.log('Hello world');
   }
+  changePin(password) {
+    this._pin = password;
+  }
+  deposit(val) {
+    this._movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  getMov() {
+    return this._movements;
+  }
 }
 
 const ivan = new Account('Ivan', 'RUB', 1111);
+console.log(ivan.changePin(2222));
 console.log(ivan);
+ivan.deposit(500);
+ivan.withdraw(200);
+console.log(ivan);
+console.log(ivan.getMov());
