96c356baee29abf38822f173c32edd8dfd150853
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-09 17:08:49 +0300

    12.4

--- script.js
+++ script.js
@@ -28,16 +28,30 @@ navigator.geolocation.getCurrentPosition(
     const { latitude, longitude } = position.coords;
     const coords = [latitude, longitude];
     const map = L.map('map').setView(coords, 13);
+    console.log(map);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup();
+    map.on('click', function (mapEvent) {
+      console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'mark-popup',
+          })
+        )
+        .setPopupContent('Trening')
+        .openPopup();
+    });
   },
   function () {
     alert('Вы не предоставили доступ к своей локации');
