ec67af39fd8563e7ca66a76ed735f497406d2f6a
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-13 13:55:07 +0300

    13.2

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-setTimeout(function () {
-  console.log('Hello');
-}, 2000);
+const request = new XMLHttpRequest();
+request.open('GET', 'https://meowfacts.herokuapp.com/?count=3');
+request.send();
+request.addEventListener('load', function () {
+  const data = JSON.parse(request.responseText);
+  console.log(data);
+  const [text1, text2, text3] = data.data;
+  console.log(text1);
+  console.log(text2);
+  console.log(text3);
+});
 
-console.log('Hello sync');
+console.log('Hello');
