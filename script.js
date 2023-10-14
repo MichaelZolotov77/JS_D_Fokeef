237455546a21b561f40947310b5ac86f3428eeea
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-14 21:57:39 +0300

    13.12.1

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 'use strict';
 
-const position = new Promise(function (result, reject) {
-  navigator.geolocation.getCurrentPosition(
-    function (pos) {
-      result(pos);
-    },
-    function (err) {
-      reject(err);
-    }
-  );
-});
-
-position.then(function (data) {
+new Promise(function (result, reject) {
+  navigator.geolocation.getCurrentPosition(result, reject);
+}).then(function (data) {
   console.log(data);
 });
