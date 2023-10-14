857897bc6feaf90feec70485e470346acb07359e
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-14 21:37:01 +0300

    13.11

--- script.js
+++ script.js
@@ -1,61 +1,18 @@
 'use strict';
 
-const lotteryTicket = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      resolve('Win');
-    } else {
-      reject('lose');
-    }
-  }, 2000);
-});
-
-lotteryTicket
-  .then(function (res) {
-    console.log(lotteryTicket);
-    console.log(res);
-  })
-  .catch(function (err) {
-    console.error(err);
-  });
-
-console.log(lotteryTicket);
+console.log('Test start');
 
 setTimeout(function () {
-  console.log('Прошла 1 сек');
-  setTimeout(function () {
-    console.log('Прошла 2 сек');
-    setTimeout(function () {
-      console.log('Прошла 3 сек');
-      setTimeout(function () {
-        console.log('Прошла 4 сек');
-        setTimeout(function () {
-          console.log('Прошла 5 сек');
-          setTimeout(function () {
-            console.log('Прошла 6 сек');
-          }, 1000);
-        }, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+  console.log('0 sec timer');
+}, 0);
 
-function wait(seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
-  });
-}
+Promise.resolve('Resolved promose 1').then(function (res) {
+  for (let i = 0; i < 2000000000; i++) {}
+  console.log(res);
+});
 
-wait(2)
-  .then(function () {
-    console.log('Вы ждали 2 секунды');
-    return wait(1);
-  })
-  .then(function () {
-    console.log('Вы ждали еще 1 секунду');
-    return wait(1);
-  })
-  .then(function () {
-    console.log('Вы ждали еще 1 секунду');
-    return wait(1);
-  });
+Promise.resolve('Resolved promose 2').then(function (res) {
+  console.log(res);
+});
+
+console.log('Test end');
