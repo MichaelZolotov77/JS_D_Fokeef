469e0d7e6ba053f3b5e67bcab0dc4b6d5d5d5d86
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-15 18:54:34 +0300

    13.13

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 'use strict';
 
-new Promise(function (result, reject) {
-  navigator.geolocation.getCurrentPosition(result, reject);
-}).then(function (data) {
+async function getCountry(country) {
+  // fetch(`https://restcountries.com/v3.1/name/${country}`)
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(data));
+
+  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+  console.log(res);
+  const data = await res.json();
   console.log(data);
-});
+}
+console.log('Hello');
+getCountry('usa');
