a0a8336e41ce41f8b10ea03d2c3df5b666ffde0d
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-15 19:40:56 +0300

    13.14

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 'use strict';
 
+const btn = document.querySelector('.btn-country');
+
 async function getCountry(country) {
   // fetch(`https://restcountries.com/v3.1/name/${country}`)
   //   .then((response) => response.json())
   //   .then((data) => console.log(data));
-
-  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-  console.log(res);
-  const data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+    if (!res.ok) {
+      throw new Error('Неправильное имя');
+    }
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
-console.log('Hello');
-getCountry('usa');
+
+btn.addEventListener('click', function () {
+  getCountry('usaaa');
+});
