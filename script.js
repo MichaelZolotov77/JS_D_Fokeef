ce6f6a9a79a5124bfd81e86f64b89704f9188c41
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-16 18:51:27 +0300

    13.16

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 'use strict';
 
-async function getCountry(country) {
-  // fetch(`https://restcountries.com/v3.1/name/${country}`)
-  //   .then((response) => response.json())
-  //   .then((data) => console.log(data));
+async function get3Capital(c1, c2, c3) {
   try {
-    const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-    if (!res.ok) {
-      throw new Error('Неправильное имя');
-    }
-    const data = await res.json();
-    return `Ваша страна ${data[0].name.common}`;
-  } catch (err) {
-    console.log(err);
-    throw new Error('Что то не так');
-  }
+    // const response1 = await fetch(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data1] = await response1.json();
+
+    // const response2 = await fetch(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data2] = await response2.json();
+
+    // const response3 = await fetch(`https://restcountries.com/v3.1/name/${c3}`);
+    // const [data3] = await response3.json();
+
+    const data = await Promise.all([
+      fetch(`https://restcountries.com/v3.1/name/${c1}`).then(function (resp) {
+        return resp.json();
+      }),
+      fetch(`https://restcountries.com/v3.1/name/${c2}`).then(function (resp) {
+        return resp.json();
+      }),
+      fetch(`https://restcountries.com/v3.1/name/${c3}`).then(function (resp) {
+        return resp.json();
+      }),
+    ]);
+
+    // console.log(data1.capital, data2.capital, data3.capital);
+    console.log(
+      data.map(function (val) {
+        return val[0].capital;
+      })
+    );
+  } catch (err) {}
 }
 
-const city = getCountry('usa');
-console.log(city);
-
-getCountry('argentina')
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
-
-(async function () {
-  try {
-    const city = await getCountry('russia');
-    console.log(city);
-  } catch (err) {
-    console.log(err);
-  }
-})();
+get3Capital('russia', 'turkey', 'thailand');
