4d0bf9851eaa465d128a2b11370a6b5a7480dfc0
MichaelZolotov77 <michaelzol@yandex.ru>
2023-10-16 19:24:03 +0300

    13.17

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Promice.all()
 async function get3Capital(c1, c2, c3) {
   try {
     // const response1 = await fetch(`https://restcountries.com/v3.1/name/${c1}`);
@@ -24,12 +25,71 @@ async function get3Capital(c1, c2, c3) {
     ]);
 
     // console.log(data1.capital, data2.capital, data3.capital);
-    console.log(
-      data.map(function (val) {
-        return val[0].capital;
-      })
-    );
   } catch (err) {}
 }
 
 get3Capital('russia', 'turkey', 'thailand');
+
+// Promice.race()
+(async function () {
+  const res = await Promise.race([
+    fetch(`https://restcountries.com/v3.1/name/usa`).then(function (resp) {
+      return resp.json();
+    }),
+    fetch(`https://restcountries.com/v3.1/name/russia`).then(function (resp) {
+      return resp.json();
+    }),
+    fetch(`https://restcountries.com/v3.1/name/thailand`).then(function (resp) {
+      return resp.json();
+    }),
+  ]);
+})();
+
+function timeout(sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Ожидание превысило ${sec} сек.`));
+    }, sec * 1000);
+  });
+}
+
+Promise.race([
+  fetch(`https://restcountries.com/v3.1/name/usa`).then(function (resp) {
+    return resp.json();
+  }),
+  timeout(0.4),
+])
+  .then(function (res) {
+    console.log(res[0]);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+// Promise.allSettled()
+
+Promise.allSettled([
+  Promise.resolve('Выполнен'),
+  Promise.reject('Отклонен'),
+  Promise.resolve('Еще один Выполнен'),
+]).then(function (res) {
+  console.log(res);
+});
+
+// Promise.all([
+//   Promise.resolve('Выполнен'),
+//   Promise.reject('Отклонен'),
+//   Promise.resolve('Еще один Выполнен'),
+// ]).then(function (res) {
+//   console.log(res);
+// });
+
+// Promise.any()
+
+Promise.any([
+  Promise.resolve('Выполнен'),
+  Promise.reject('Отклонен'),
+  Promise.resolve('Еще один Выполнен'),
+]).then(function (res) {
+  console.log(res);
+});
